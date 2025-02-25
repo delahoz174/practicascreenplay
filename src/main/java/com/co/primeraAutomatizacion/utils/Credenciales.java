7003bcced20842354f4f91ec7963ddeed63d1d58
delahoz174 <delahoz174@hotmail.com>
2025-02-24 20:47:40 -0500

    se agrega una nueva variable CLAVE

--- src/main/java/com/co/primeraAutomatizacion/utils/Credenciales.java
+++ src/main/java/com/co/primeraAutomatizacion/utils/Credenciales.java
@@ -4,4 +4,5 @@ public class Credenciales {
     public static final String URL = "https://www.demoblaze.com/";
     public static final String USUARIO = "admin";
     public static final String CLAVE = "admin";
+    public static final String CLAVE2 = "admin";
 }
